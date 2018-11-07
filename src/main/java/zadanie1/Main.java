cc9595cc1bd2c1d52b736154f680e95af986af39
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-07 19:58:39 +0100

    dodanie pierwszego zadania z zakresu kolekcji

---
+++ src/main/java/zadanie1/Main.java
@@ -0,0 +1,25 @@
+package zadanie1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        List<Integer> integerList = new ArrayList<Integer>();
+        for (int i=0; i<5; i++){
+            System.out.println("Podaj " + (int)(i+1) + ". liczbę");
+            integerList.add(scanner.nextInt());
+        }
+        for (int i=0; i<5; i++){
+            integerList.add(random.nextInt(100));
+        }
+        for (Integer z: integerList){
+            System.out.println(z);
+        }
+    }
+}

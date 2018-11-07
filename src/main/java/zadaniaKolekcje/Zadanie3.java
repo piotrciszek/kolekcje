41792e923fedfb29ad9954233276c73ff3bf57b8
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2018-11-07 20:49:57 +0100

    Zadanie 3 dodane

--- src/main/java/zadaniaKolekcje/Zadanie3.java
+++ src/main/java/zadaniaKolekcje/Zadanie3.java
@@ -8,11 +8,20 @@ public class Zadanie3 {
 
     public static void main(String[] args) {
         List<String> lista = new ArrayList<>();
-        lista.add(Arrays.asList(10030, 3004, 4000, 12355, 12222, 67888, 111200, 225355, 2222, 1111, 3546, 138751, 235912).toString());
+        lista.add("10030");
+        lista.add("3004");
+        lista.add("4000");
+        lista.add("67888");
+        lista.add("138751");
+//        lista.add(Arrays.asList(10030, 3004, 4000, 12355, 12222, 67888, 111200, 225355, 2222, 1111, 3546, 138751, 235912).toString());
 
 
         System.out.println("Indeks elementu 138751 to: " + lista.indexOf("138751"));
-        System.out.println(lista.contains("67888"));
+        if (lista.contains("67888")){
+            System.out.println("tak");
+        } else {
+            System.out.println("nie");
+        }
         System.out.println(lista.contains("67889"));
         lista.remove("67888");
         lista.remove("67889");
